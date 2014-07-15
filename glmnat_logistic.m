628105e2de1ad6b0483e95ba445e19539a974f2f
tom.michoel <tom.michoel@gmail.com>
2014-07-15 10:15:46 +0000

    Initial code and data upload

---
+++ glmnat_logistic.m
@@ -0,0 +1,80 @@
+function fit = glmnat_logistic(varargin)
+% GLMNAT_LOGISTIC - Natural coordinate descent algorithm for logistic regression
+% GLMNAT_LOGISTIC runs the natural coordinate descent algorithm for
+% logistic regression (see reference below). 
+%
+% INSTALLATION : This function is a wrapper for the C-MEX function
+%   GLMNAT_LOGISTIC_SRC.C which needs to be compiled first. Compilation
+%   instructions are provided in the header of this C-file.
+% 
+% USAGE: [alpha,beta,lambda] = glmnat_logistic(X,Y);
+%        [alpha,beta,lambda] = glmnat_logistic(X,Y,lambda);
+%        [alpha,beta,lambda] = glmnat_logistic(X,Y,lambda,epsconv);
+%
+% INPUT: X       - NxP matrix of predictor data (N samples, P predictors).
+%        Y       - Nx1 vector of response data.
+%        lambda  - Sequence of regularization parameters, sort from
+%                  high to low for fastest results. Optional parameter, default
+%                  is to use a geometric series of length 100.
+%        epsconv - Convergence threshold. Optional parameter, default value
+%                  1E-3.
+%
+% OUTPUT: fit         - Structure with results:
+%         fit.a0      - Intercept parameter.
+%         fit.beta    - Regression coefficients for predictor variables
+%         fit.lambda  - Sequence of regularization parameters
+%         fit.epsconv - Convergence threshold
+%
+% AUTHOR : Tom Michoel, The Roslin Institute
+%          tom.michoel@roslin.ed.ac.uk
+%          http://lab.michoel.info
+% 
+% REFERENCE: Michoel T. Natural coordinate descent algorithm for 
+%  L1-penalised regression in generalised linear models. arXiv:1405.4225
+%
+% LICENSE: GNU GPL v2
+
+X = varargin{1}; % predictor data
+Y = varargin{2}; % response data
+
+% sanity checks on the data
+if numel(Y)~=length(Y)
+    error('Response data Y is not a vector.')
+end
+if size(X,1)~=size(Y,1)
+    error('Number of samples in predictor X and response Y are not identical.')
+end
+
+% standardize X and add column of 1s for intercept
+X2 = [ones(size(Y)) zscore(X,1)];
+
+% overlap vector
+w = (X2'*Y)/length(Y);
+
+% default geometric regularization parameter sequence
+nlambda = 100;
+lambdaratio = 1e-2;
+lambdamax = max(w(2:end));
+nseq = (nlambda:-1:1)';
+lambda = lambdamax * lambdaratio.^((nlambda-nseq)/(nlambda-1));
+
+% default convergence threshold
+epsconv = 1e-3;
+
+% override regularization parameter and convergence threshold if given
+switch nargin
+    case 3
+        lambda = varargin{3}; % sequence of penalty parameters
+    case 4
+        lambda = varargin{3}; % sequence of penalty parameters
+        epsconv = varargin{4}; % convergence parameter
+end
+
+% run logistic regression
+beta = glmnat_logistic_src(X2,w,lambda,epsconv);
+
+% return output structure
+fit.a0      = beta(1,:);     % intercept
+fit.beta    = beta(2:end,:); % regression coefficients
+fit.lambda  = lambda;        % regularization parameter sequence
+fit.epsconv = epsconv;       % convergence threshold
---
+++ glmnat_logisticscore.m
@@ -0,0 +1,36 @@
+function score = glmnat_logisticscore(X,Y,fit)
+% GLMNAT_LOGISTICSCORE - Compute logistic regression cost function
+% GLMNAT_LOGISTICSCORE computes the logistic regression cost function for a
+% set of regression results. This function is primarily used to compare the
+% output of different logistic regression algorithms.
+%
+% USAGE: score = glmnat_logisticscore(X,Y,alpha,beta,lambda);
+%
+% INPUT: X   - NxP matrix of predictor data (N samples, P predictors).
+%        Y   - Nx1 vector of response data.
+%        fit - structure with fields
+%              fit.a0     - 1xK vector of intercept parameters.
+%              fit.beta   - PxK matrix of regression coefficients
+%              fit.lambda - Kx1 vector of regularization parameters.
+%
+% OUTPUT: score - Kx1 vector of cost function values
+%
+% AUTHOR : Tom Michoel, The Roslin Institute
+%          tom.michoel@roslin.ed.ac.uk
+%          http://lab.michoel.info
+% 
+% REFERENCE: Michoel T. Natural coordinate descent algorithm for 
+%  L1-penalised regression in generalised linear models. arXiv:1405.4225
+%
+% LICENSE: GNU GPL v2
+
+
+X2 = [ones(size(Y)) zscore(X,1)]; % standardize X and add intercept
+w = (X2'*Y)/length(Y); % overlap vector
+beta = [fit.a0; fit.beta]; % merge intercept and regression coefficients
+
+score = zeros(size(fit.lambda)); 
+for k=1:length(score)
+    score(k) = mean(log(1+exp(X2*beta(:,k)))) - w'*beta(:,k) ...
+        + fit.lambda(k)*sum(abs(beta(2:end,k)));
+end
